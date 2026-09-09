170514f5af807099c3a4a6b5e57e78af5746b955
Kevin Shehu <57314395+kevinshehu@users.noreply.github.com>
2026-09-09 12:28:19 +0200

    Fix SSR build for Netlify static hosting

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -5,11 +5,6 @@ import {
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  provideClientHydration,
-  withEventReplay,
-  withIncrementalHydration,
-} from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { appRoutes } from './app-routes';
@@ -17,7 +12,6 @@ import { appRoutes } from './app-routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withComponentInputBinding()),
-    provideClientHydration(withEventReplay(), withIncrementalHydration()),
     provideHttpClient(withFetch()),
     importProvidersFrom(BrowserModule),
     { provide: LOCALE_ID, useValue: 'en-US' },
